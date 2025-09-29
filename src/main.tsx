e2afd72009f0423b7c362fe522aa7606cf9a7ff2
devpedropipa <phpipa2006@gmail.com>
2025-09-29 20:17:55 -0300

    criando um arquivo para tornar o site responsivo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css"; /* Configuração geral */
+import "./styles/media-queries.css"; /* Torna o site responsivo */
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
